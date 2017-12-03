f32bcc36854af03b0663c6e0d9d9d7ec3729a994
Jonah Ollman <jonahollman@gmail.com>
2017-12-02 16:07:48 -0800

    Add favorites architecture

--- tacofinder/FavoritesViewController.swift
+++ tacofinder/FavoritesViewController.swift
@@ -7,26 +7,50 @@
 //
 
 import UIKit
+import CoreLocation
 
-class FavoritesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class FavoritesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet var favoritesTable: UITableView!
+    var favorites = [String]()
+    var favoriteLats = [CLLocationDegrees]()
+    var favoriteLongs = [CLLocationDegrees]()
+    
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if UserDefaults.standard.object(forKey: "favorites") != nil {
+            favorites = UserDefaults.standard.object(forKey: "favorites") as! [String]
+            favoriteLats = UserDefaults.standard.object(forKey: "favoriteLats") as! [CLLocationDegrees]
+            favoriteLongs = UserDefaults.standard.object(forKey: "favoriteLongs") as! [CLLocationDegrees]
+        }
+        
         favoritesTable.delegate = self
         favoritesTable.dataSource = self
+        locationManager.delegate = self
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        
+        DispatchQueue.main.async {
+            self.locationManager.startUpdatingLocation()
+        }
 
-        // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return favorites.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = favoritesTable.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = favorites[indexPath.row]
+        
+        let tacoLocation = CLLocation(latitude: favoriteLats[indexPath.row], longitude: favoriteLongs[indexPath.row])
+        let userLocation = locationManager.location
+        cell.detailTextLabel?.text = "\(round(10*(userLocation?.distance(from: tacoLocation))!/1609)/10) miles away"
         
         return cell
     }
--- tacofinder/ResultViewController.swift
+++ tacofinder/ResultViewController.swift
@@ -26,36 +26,50 @@ class ResultViewController: UIViewController, MKMapViewDelegate, CLLocationManag
     var locationManager = CLLocationManager()
     var tacoResults = [CDYelpBusiness]()
     var tacoLocation: CDYelpCoordinates?
+    var resultNumber = 0
+    var favorites = [String]()
+    var favoriteLats = [CLLocationDegrees]()
+    var favoriteLongs = [CLLocationDegrees]()
+    var isFavorite = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        
+        if UserDefaults.standard.object(forKey: "favorites") != nil && UserDefaults.standard.object(forKey: "favoriteLats") != nil && UserDefaults.standard.object(forKey: "favoriteLongs") != nil {
+            favorites = UserDefaults.standard.object(forKey: "favorites") as! [String]
+            favoriteLats = UserDefaults.standard.object(forKey: "favoriteLats") as! [CLLocationDegrees]
+            favoriteLongs = UserDefaults.standard.object(forKey: "favoriteLongs") as! [CLLocationDegrees]
+        }
 
-        setupFirstResult()
+        setupResult()
+        
+        setupFavoritesIcon()
         
     }
     
-    func setupFirstResult() {
-        let firstTaco = tacoResults[1]
-        self.tacoName.text = firstTaco.name
-        self.tacoAddress.text = """
-            \(firstTaco.location!.addressOne ?? "")
-            \(firstTaco.location!.addressTwo ?? "")
-            \(firstTaco.location!.addressThree ?? "")
+    func setupResult() {
+        let result = tacoResults[resultNumber]
+        DispatchQueue.main.async {
+            self.tacoName.text = result.name
+            self.tacoAddress.text = """
+            \(result.location!.addressOne ?? "")
+            \(result.location!.addressTwo ?? "")
+            \(result.location!.addressThree ?? "")
             """
-       // self.tacoHours.text = firstTaco.hours![0].open![0].end as? String
-        self.tacoPhone.setTitle(firstTaco.displayPhone, for: .normal)
-        self.tacoLocation = firstTaco.coordinates
-        setupMap(location: firstTaco.coordinates!)
+            // self.tacoHours.text = result![0].open![0].end as? String
+            self.tacoPhone.setTitle(result.displayPhone, for: .normal)
+        }
+        self.tacoLocation = result.coordinates
+        setupMap(location: result.coordinates!)
+        checkIfFavorite()
     }
     
     func setupMap(location: CDYelpCoordinates) {
         locationMap.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.delegate = self
-        
-        DispatchQueue.main.async {
-            self.locationManager.startUpdatingLocation()
+        if self.locationMap.annotations.count != 0 {
+            self.locationMap.removeAnnotation(self.locationMap.annotations[0])
         }
         
         let center = CLLocationCoordinate2D(latitude: (location.latitude)!, longitude: (location.longitude)!)
@@ -63,18 +77,65 @@ class ResultViewController: UIViewController, MKMapViewDelegate, CLLocationManag
         pin.coordinate = center
         locationMap.addAnnotation(pin)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        locationMap.setRegion(region, animated: false)
+        locationMap.setRegion(region, animated: true)
+    }
+    
+    func setupFavoritesIcon() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(changeFavoritesStatus))
+        self.favoritesIcon.isUserInteractionEnabled = true
+        self.favoritesIcon.addGestureRecognizer(tap)
+    }
+    
+    func checkIfFavorite() {
+        print(favorites)
+        for favorite in favorites {
+            if favorite == tacoResults[resultNumber].name {
+                self.favoritesIcon.image = UIImage(named: "heart-outline")
+                self.isFavorite = true
+                break
+            } else {
+                self.favoritesIcon.image = UIImage(named: "heart-outline-plus")
+                self.isFavorite = false
+            }
+        }
+    }
+    
+    @objc func changeFavoritesStatus() {
+        if self.isFavorite {
+            self.favoritesIcon.image = UIImage(named: "heart-outline-plus")
+            self.isFavorite = false
+            let index = self.favorites.index(of: self.tacoName.text!)
+            self.favorites.remove(at: index!)
+            self.favoriteLats.remove(at: index!)
+            self.favoriteLongs.remove(at: index!)
+            print(favorites)
+        } else {
+            self.favoritesIcon.image = UIImage(named: "heart-outline")
+            self.isFavorite = true
+            self.favorites.append(self.tacoName.text!)
+            self.favoriteLats.append((self.tacoLocation?.latitude!)!)
+            self.favoriteLongs.append((self.tacoLocation?.longitude)!)
+            print(favorites)
+        }
+        updateUserDefaults()
+    }
+    
+    func updateUserDefaults() {
+        UserDefaults.standard.set(self.favorites, forKey: "favorites")
+        UserDefaults.standard.set(self.favoriteLats, forKey: "favoriteLats")
+        UserDefaults.standard.set(self.favoriteLongs, forKey: "favoriteLongs")
+
     }
     
     @IBAction func goToTaco(_ sender: Any) {
     }
     
     @IBAction func nextResult(_ sender: Any) {
+        self.resultNumber += 1
+        print("result number: \(resultNumber)")
+        setupResult()
     }
-    
-    func addToFavorites() {
-        
-    }
+
     
     @IBAction func goToFavorites(_ sender: Any) {
         let favorites = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "favorites")
